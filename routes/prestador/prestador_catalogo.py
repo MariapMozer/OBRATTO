3fcf8ae34f01aad94d64f6e3f91fbe123f9c146b
gabriellyvidal <gabriellyvidal.ifes@gmail.com>
2025-09-20 11:25:02 -0300

    Cliente ajustes

---
+++ routes/prestador/prestador_catalogo.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request, Form, HTTPException, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from typing import Optional, List
+from config import templates
+from utils.auth_decorator import requer_autenticacao
+
+router = APIRouter()
+
+# rota catálogo de prestadores
+@router.get("/catalogo", name="catalogo_prestadores")
+async def catalogo_prestadores(request: Request):
+    return templates.TemplateResponse(
+        "prestador/catalogo/catalogo_prestadores.html",
+        {"request": request}
+    )
+
