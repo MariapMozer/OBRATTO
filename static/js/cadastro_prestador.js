a763858694023b43f1932e1305582a0e8cc900bf
millena gualandi <millenagualandi@gmail.com>
2025-09-22 18:22:57 -0300

    form prestador

--- static/js/cadastro_prestador.js
+++ static/js/cadastro_prestador.js
@@ -41,13 +41,13 @@
         }
 
         // Máscaras para CPF e telefone
-        document.getElementById('cpf').addEventListener('input', function(e) {
-            let value = e.target.value.replace(/\D/g, '');
-            value = value.replace(/(\d{3})(\d)/, '$1.$2');
-            value = value.replace(/(\d{3})(\d)/, '$1.$2');
-            value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-            e.target.value = value;
-        });
+        // document.getElementById('cpf').addEventListener('input', function(e) {
+        //     let value = e.target.value.replace(/\D/g, '');
+        //     value = value.replace(/(\d{3})(\d)/, '$1.$2');
+        //     value = value.replace(/(\d{3})(\d)/, '$1.$2');
+        //     value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        //     e.target.value = value;
+        // });
 
         document.getElementById('telefone').addEventListener('input', function(e) {
             let value = e.target.value.replace(/\D/g, '');
